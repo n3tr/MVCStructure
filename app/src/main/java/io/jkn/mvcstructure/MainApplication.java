944a93f18166df847df788142bc46a74590a8f91
n3tr <saakyz@gmail.com>
2014-12-25 11:29:58 +0700

    Add screen utils

--- app/src/main/java/io/jkn/mvcstructure/MainApplication.java
+++ app/src/main/java/io/jkn/mvcstructure/MainApplication.java
@@ -2,6 +2,8 @@ package io.jkn.mvcstructure;
 
 import android.app.Application;
 
+import com.inthecheesefactory.thecheeselibrary.manager.Contextor;
+
 /**
  * Created by n3tr on 12/25/2014 AD.
  */
@@ -10,5 +12,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Contextor.getInstance().init(getApplicationContext());
     }
 }
